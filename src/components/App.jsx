5f0722f8da31d3e518c85c22ae7b9c38b91f6925
andreaweb <deiainluv@gmail.com>
2019-01-26 07:50:22 -0200

    basic config of project

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  state = {
+    
+  };
+
+  render() {
+    return (
+        <div className="container">
+          
+        </div>
+    );
+  }
+}
+
+App.propTypes = {
+
+};
+
+export default App;
